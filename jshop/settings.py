a6fba536f0f19ca3f8bf883758418dc36cc9adfe
Jonathan Desplats <124144015+joninet@users.noreply.github.com>
2024-11-27 18:44:50 +0000

    ok

--- jshop/settings.py
+++ jshop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,18 +25,23 @@ SECRET_KEY = "django-insecure-vl%_yenn%b_f#-1$f1d3hnc@x7nfga=9v&w#22pk5#nd=%n!ah
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    "web",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+]
+CSRF_TRUSTED_ORIGINS = [
+    'https://zany-happiness-g9vxrwgqvv4cwwqg-8000.app.github.dev/',
+    'http://zany-happiness-g9vxrwgqvv4cwwqg-8000.app.github.dev/'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +126,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+
