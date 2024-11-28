167db44f18b3c49a642bfc871983afe75c92cc83
joninet2971 <j.desplats@itecriocuarto.org.ar>
2024-11-27 21:47:56 -0300

    modlulo 1

--- web/views.py
+++ web/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from .models import Categoria, Producto
 # Create your views here.
+
+"""vistas para el catalogo de productos"""
+def index(request):
+    listaProductos = Producto.objects.all()
+    context = {
+        'productos':listaProductos
+    }
+
+    return render(request,'index.html', context)
